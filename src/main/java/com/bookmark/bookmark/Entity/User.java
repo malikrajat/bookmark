5860d045bd8aeaabe991cfb97a1d39d75fd27730
malikrajat <er.rajatmalik@gmail.com>
2021-11-11 22:32:05 +0530

    update user entity

--- src/main/java/com/bookmark/bookmark/Entity/User.java
+++ src/main/java/com/bookmark/bookmark/Entity/User.java
@@ -26,64 +26,64 @@ import lombok.Data;
 @Data
 public class User implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(unique = true)
-	@NotNull
-	long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true)
+    @NotNull
+    long id;
 
-	@Column(nullable = false, unique = true, length = 100)
-	@NotBlank(message = "Slug is missing.")
-	@Size(min = 3, max = 30, message = "Slug must be between 3 to 30 !!.")
-	private String slug;
+    @Column(nullable = false, unique = true, length = 100)
+    @NotBlank(message = "Slug is missing.")
+    @Size(min = 3, max = 30, message = "Slug must be between 3 to 30 !!.")
+    private String slug;
 
-	@Column(name = "user_name", nullable = false, unique = true, length = 100, columnDefinition = "varchar(50)  default ''")
-	@NotBlank(message = "Please enter user Name")
-	@Size(min = 3, max = 12, message = "User Name must be between 3 to 12 !!.")
-	private String userName;
+    @Column(name = "user_name", nullable = false, unique = true, length = 100, columnDefinition = "varchar(50)  default ''")
+    @NotBlank(message = "Please enter user Name")
+    @Size(min = 3, max = 12, message = "User Name must be between 3 to 12 !!.")
+    private String userName;
 
-	@Column(nullable = false, unique = false, length = 100, columnDefinition = "varchar(100)  default ''")
-	@NotBlank(message = "Please enter name")
-	@Size(min = 3, max = 20, message = "Name must be between 3 to 20 !!.")
-	private String name;
+    @Column(nullable = false, unique = false, length = 100, columnDefinition = "varchar(100)  default ''")
+    @NotBlank(message = "Please enter name")
+    @Size(min = 3, max = 20, message = "Name must be between 3 to 20 !!.")
+    private String name;
 
-	@Column(nullable = false, unique = true, length = 100, columnDefinition = "varchar(100)  default ''")
-	@NotBlank(message = "Email can not be empty !!.")
-	@Size(min = 3, max = 20, message = "Email must be between 3 to 20 !!.")
-	@Email(message="Please enter correct email address")
-	private String email;
+    @Column(nullable = false, unique = true, length = 100, columnDefinition = "varchar(100)  default ''")
+    @NotBlank(message = "Email can not be empty !!.")
+    @Size(min = 3, max = 20, message = "Email must be between 3 to 20 !!.")
+    @Email(message = "Please enter correct email address")
+    private String email;
 
-	@Size(min = 3, max = 20, message = "Password must be between 3 to 20 !!.")
-	@NotBlank(message = "Please enter password !!.")
-	@Column(nullable = false, unique = false, length = 100, columnDefinition = "varchar(100)  default ''")
-	private String password;
+    @Size(min = 3, max = 20, message = "Password must be between 3 to 20 !!.")
+    @NotBlank(message = "Please enter password !!.")
+    @Column(nullable = false, unique = false, length = 100, columnDefinition = "varchar(100)  default ''")
+    private String password;
 
-	@Column(name = "term_condition", length = 1, columnDefinition = "boolean  default 0")
-	@AssertTrue(message="please accept term & consition")
-	private Boolean termCondition;
+    @Column(name = "term_condition", length = 1, columnDefinition = "boolean  default false")
+    @AssertTrue(message = "please accept term & condition.")
+    private Boolean termCondition;
 
-	@Column(name = "is_active", length = 1, columnDefinition = "boolean  default 0")
-	private Boolean isActive;
+    @Column(name = "is_active", length = 1, columnDefinition = "boolean  default 0")
+    private Boolean active;
 
-	@Column(name = "is_deleted", length = 1, columnDefinition = "boolean  default 0")
-	private Boolean isDeleted;
-	
-	@Column(name = "is_blocked", length = 1, columnDefinition = "boolean  default 0")
-	private Boolean isBlocked;
+    @Column(name = "is_deleted", length = 1, columnDefinition = "boolean  default 0")
+    private Boolean deleted;
 
-	@Column(name = "email_token", length = 1, nullable = true, unique = false, columnDefinition = "varchar(100)  default ''")
-	private String emailToken;
-	
-	@Column(name = "last_login", length = 1, nullable = true, unique = false, columnDefinition = "Date")
-	private String lastLogin;
+    @Column(name = "is_blocked", length = 1, columnDefinition = "boolean  default 0")
+    private Boolean blocked;
 
-	@Column(nullable = false, updatable = false)
-	@CreationTimestamp
-	private Date created;
-	
-	@UpdateTimestamp
-	private Date updated;
+    @Column(name = "email_token", length = 1, nullable = true, unique = false, columnDefinition = "varchar(100)  default ''")
+    private String emailToken;
+
+    @Column(name = "last_login", length = 1, nullable = true, unique = false, columnDefinition = "DateTime")
+    private String lastLogin;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private Date created;
+
+    @UpdateTimestamp
+    private Date updated;
 
 }
