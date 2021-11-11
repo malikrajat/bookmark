47db61844444e36f9a25aae4b181f05ee86ac1f7
malikrajat <er.rajatmalik@gmail.com>
2021-11-11 19:15:35 +0530

    first commit

---
+++ src/test/java/com/bookmark/bookmark/BookmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmark.bookmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
