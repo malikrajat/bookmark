3ef2c5e4ea72af02ccbc5cf7535135a5aac4b043
malikrajat <er.rajatmalik@gmail.com>
2021-11-11 23:19:08 +0530

    user controller created

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import org.slf4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/user")
+public class UserController {
+
+	private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(UserController.class);
+
+	@GetMapping("/")
+	public ResponseEntity<HttpStatus> home() {
+		LOGGER.info("Home page loaded ");
+		return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+	}
+
+}
