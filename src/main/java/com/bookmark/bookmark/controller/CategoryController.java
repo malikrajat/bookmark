fd822ed5980c639e2b0b5fcbc935388ea40a7b22
malikrajat <er.rajatmalik@gmail.com>
2021-11-26 23:09:52 +0530

    CategoryController

--- src/main/java/com/bookmark/bookmark/controller/CategoryController.java
+++ src/main/java/com/bookmark/bookmark/controller/CategoryController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,21 +26,41 @@ import com.bookmark.bookmark.service.UserService;
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/category")
-public class CatrgoryController {
+public class CategoryController {
 
     /**
      * Logging service
      */
-    private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(CollectionController.class);
+	
+    private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(CategoryController.class);
 
     /**
      * User related logic service
      */
+    
     @Autowired
     private UserService userService;
     
     @Autowired
     private CategoryService categoryService;
+    
+    /**
+     * This method get book mark by id and it required
+     * authorization
+     * 
+     * 
+     * @param none
+     * @return ok status
+     * @throws exception
+     * 
+     */
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getBookMarkByCategoryId(@PathVariable("id") long id ) throws Exception {
+        LOGGER.info("getBookMarkListByCategory page loaded ");
+        Category categoryList = this.categoryService.getCategoryById(id);
+        return ResponseEntity.ok(categoryList);
+    }
     
     /**
      * This method get category and it required
@@ -51,11 +72,31 @@ public class CatrgoryController {
      * @throws exception
      * 
      */
-    @GetMapping("/")
+    
+    @GetMapping("/list")
     public ResponseEntity<List<Category>> home() throws Exception {
         LOGGER.info("collection page loaded ");
         User user = this.userService.getDefaultUser();
         List<Category> categoryList = this.categoryService.getCategoryListBasedOnLoginUser(user.getId());
         return ResponseEntity.ok(categoryList);
     }
+    
+    /**
+     * This method get category based on collection id and it required
+     * authorization
+     * 
+     * 
+     * @param none
+     * @return ok status
+     * @throws exception
+     * 
+     */
+    
+    @GetMapping("/collection/{collectionId}")
+    public ResponseEntity<List<Category>> getCategoryByCollection(@PathVariable("collectionId") long collectionId ) throws Exception {
+        LOGGER.info("getCategoryByCollection page loaded ");
+        List<Category> categoryList = this.categoryService.getCategoryListBasedOnCollection(collectionId);
+        return ResponseEntity.ok(categoryList);
+    }
+    
 }
